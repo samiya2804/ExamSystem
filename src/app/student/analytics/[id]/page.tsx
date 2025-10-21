66ae5425136f01ed5ae674c92ac288b6ecf6e1a6
Mohammad Iqbal <ki925053@gmail.com>
2025-10-21 23:24:24 +0530

    added analytics model url

--- src/app/student/analytics/[id]/page.tsx
+++ src/app/student/analytics/[id]/page.tsx
@@ -26,7 +26,7 @@ export default function AnalyticsPage() {
       <h1 className="text-2xl font-bold text-white mb-4">Student Analytics</h1>
 
       <iframe
-        src={`http://127.0.0.1:8000/report/${studentId}`}
+        src={`${process.env.NEXT_PUBLIC_ANALYTICS_MODEL_URL}/report/${studentId}`}
         width="100%"
         height="800"
         className="rounded-lg border border-slate-700 shadow-lg bg-white"
